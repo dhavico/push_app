438d650d211fb730fe880e46d6df586540482718
David Ortega <dhavico@hotmail.com>
2023-09-13 15:54:51 -0500

    Iniciando

---
+++ lib/presentation/screens/home_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Permisos'),
+        actions: [IconButton(onPressed: () => {}, icon: Icon(Icons.settings))],
+      ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 0,
+      itemBuilder: (context, index) {
+        return const ListTile();
+      },
+    );
+  }
+}

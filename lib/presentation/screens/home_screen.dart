d7b2ca78debda137bfc167818997fa8ea47c4d16
David Ortega <dhavico@hotmail.com>
2023-09-13 16:36:31 -0500

    avance de la seccion 21

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:push_app/presentation/blocs/notifications/notifications_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,8 +9,16 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Permisos'),
-        actions: [IconButton(onPressed: () => {}, icon: Icon(Icons.settings))],
+        title: context.select((NotificationsBloc bloc) => Text(
+              '${bloc.state.status}',
+              style: const TextStyle(fontSize: 18),
+            )),
+        actions: [
+          IconButton(
+              onPressed: () =>
+                  {context.read<NotificationsBloc>().requestPermission()},
+              icon: const Icon(Icons.settings))
+        ],
       ),
       body: const _HomeView(),
     );
